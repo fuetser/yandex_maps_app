f1109abc77bed8a79ef0995358c3f611320a0e11
Masynchin <banzur.badurov@gmail.com>
2021-02-07 15:49:38 +0300

    little style fixes

--- main.py
+++ main.py
@@ -1,50 +1,53 @@
+import sys
+
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtCore import Qt
 import requests
-import sys
 
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.current_zoom = 12  # [1; 17]
-        self.current_longitude = 39
-        self.current_latitude = 59
-        self.map_layout = "map"
+        self.current_longitude = 37.91
+        self.current_latitude = 59.13
+        self.map_layout_type = "map"
         self.server = "http://static-maps.yandex.ru/1.x/"
         self.setup_ui()
 
     def setup_ui(self):
+        self.setWindowTitle("Yandex Maps")
         self.main_layout = QtWidgets.QVBoxLayout(self)
         self.map_lable = QtWidgets.QLabel(self)
         self.main_layout.addWidget(self.map_lable)
+        self.update_map()
+
+    def update_map(self):
         self.set_map(self.get_map())
-        self.setWindowTitle("Yandex Maps")
 
     def get_map(self):
         payload = {
-            "ll": ",".join(map(
-                str, (self.current_longitude, self.current_latitude))),
-            "l": self.map_layout,
+            "ll": f"{self.current_longitude},{self.current_latitude}",
+            "l": self.map_layout_type,
             "z": str(self.current_zoom),
         }
         responce = requests.get(self.server, params=payload)
         if responce.ok:
             return responce.content
 
-    def set_map(self, bytes_io):
+    def set_map(self, image_bytes):
         image = QtGui.QPixmap()
-        image.loadFromData(bytes_io)
+        image.loadFromData(image_bytes)
         self.map_lable.setPixmap(image)
 
     def change_zoom(self, delta):
         if 1 <= self.current_zoom + delta <= 17:
             self.current_zoom += delta
-            self.set_map(self.get_map())
+            self.update_map()
 
     def keyPressEvent(self, event):
         if (key := event.key()) == Qt.Key_PageUp:
-            self.change_zoom(1)
+            self.change_zoom(+1)
         elif key == Qt.Key_PageDown:
             self.change_zoom(-1)
 
