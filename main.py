9ab8b6061f49ba572c78d7bdc783bd31d149e705
Masynchin <banzur.badurov@gmail.com>
2021-02-07 20:51:19 +0300

    add postal code enable/disable

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ class MainWindow(QtWidgets.QWidget):
         self.current_longitude = 37.91
         self.current_latitude = 59.13
         self.current_mark = None
+        self.current_address = None
+        self.current_postal_code = None
+        self.show_postal_code = False
         self.map_layout_type = "map"
         self.static_server = "http://static-maps.yandex.ru/1.x/"
         self.geocode_server = "http://geocode-maps.yandex.ru/1.x/"
@@ -52,31 +55,42 @@ class MainWindow(QtWidgets.QWidget):
         search_layout.addWidget(self.geocode_field)
         self.main_layout.addLayout(search_layout)
 
-        self.adress_field = QtWidgets.QLineEdit()
-        self.adress_field.setEnabled(False)
-        self.main_layout.addWidget(self.adress_field)
+        self.address_field = QtWidgets.QLineEdit()
+        self.address_field.setEnabled(False)
+
+        self.postal_code_button = QtWidgets.QRadioButton("Отображать почтовый адрес")
+        self.postal_code_button.toggled.connect(self.postal_button_change)
+
+        address_layout = QtWidgets.QHBoxLayout()
+        address_layout.addWidget(self.address_field)
+        address_layout.addWidget(self.postal_code_button)
+        self.main_layout.addLayout(address_layout)
 
     def change_map_layout_type(self, map_layout_type):
         self.map_layout_type = MAP_LAYOUT_TYPES[map_layout_type]
         self.update_map()
 
     def update_map(self):
-        self.set_map(self.get_map(self.current_mark))
+        self.set_map(self.get_map())
         self.setFocus()
 
     def remove_mark(self):
         self.current_mark = None
-        self.adress_field.clear()
+        self.address_field.clear()
         self.update_map()
 
-    def get_map(self, mark):
+    def postal_button_change(self, show_postal_code):
+        self.show_postal_code = show_postal_code
+        self.address_field.setText(self.format_address())
+
+    def get_map(self):
         payload = {
             "ll": f"{self.current_longitude},{self.current_latitude}",
             "l": self.map_layout_type,
             "z": str(self.current_zoom),
         }
-        if mark is not None:
-            payload["pt"] = "{},{},pm2dom".format(*mark)
+        if self.current_mark is not None:
+            payload["pt"] = "{},{},pm2dom".format(*self.current_mark)
 
         response = requests.get(self.static_server, params=payload)
         if response.ok:
@@ -90,8 +104,9 @@ class MainWindow(QtWidgets.QWidget):
     def search_place(self):
         geocode = self.geocode_field.text().strip()
         if geocode:
-            self.current_mark, adress = self.parse_geocode(geocode)
-            self.adress_field.setText(adress)
+            self.current_mark, self.current_address, \
+                self.current_postal_code = self.parse_geocode(geocode)
+            self.address_field.setText(self.format_address())
             if self.current_mark is not None:
                 self.current_longitude, self.current_latitude = self.current_mark
             self.update_map()
@@ -107,11 +122,18 @@ class MainWindow(QtWidgets.QWidget):
             geobject = response["response"][
                 "GeoObjectCollection"]["featureMember"][0]["GeoObject"]
             pos = tuple(map(float, geobject["Point"]["pos"].split()))
-            adress = geobject["metaDataProperty"]["GeocoderMetaData"]["text"]
-            return pos, adress
+            metadata = geobject["metaDataProperty"]["GeocoderMetaData"]
+            address = metadata["text"]
+            postal_code = metadata["Address"].get("postal_code")
+            return pos, address, postal_code
 
         except Exception:
-            return self.current_mark, "Не найдено"
+            return self.current_mark, "Не найдено", None
+
+    def format_address(self):
+        if self.show_postal_code and self.current_postal_code is not None:
+            return f"{self.current_address}, {self.current_postal_code}"
+        return self.current_address
 
     def keyPressEvent(self, event):
         if (key := event.key()) == Qt.Key_PageUp:
